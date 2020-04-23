d0edc33ebf99f3cbc7992d8ae5a86a3b79301324
Travis Clement <travclement@gmail.com>
2020-04-22 18:25:33 -0700

    Basic meshing with bumper control

---
+++ Unity/Chef's Table/Assets/Scripts/Control.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class Control : MonoBehaviour {
+   #region Public Variables 
+   public enum ButtonStates { 
+	   Normal, 
+	   Pressed, 
+	   JustReleased
+   };
+   public ButtonStates BtnState;  
+   #endregion
+
+   #region Private Variables
+   private const float TIME_MESH_SCANNING_TOGGLE = 3.0f;
+   private bool _held = false;
+   private float _startTime = 0.0f;
+   private MeshingScript _meshing;
+   #endregion
+
+   #region Unity Methods
+   private void Start() {
+       // Start input
+       MLInput.Start();
+
+       // Add button callbacks
+       MLInput.OnControllerButtonDown += HandleOnButtonDown;
+       MLInput.OnControllerButtonUp += HandleOnButtonUp;
+
+       // Assign meshing component
+       _meshing = GetComponent<MeshingScript>();
+
+       // Initial State of the Control is Normal
+       BtnState = ButtonStates.Normal;
+   }
+
+   private void OnDestroy() {
+       // Stop input
+       MLInput.Stop();
+
+       // Remove button callbacks
+       MLInput.OnControllerButtonDown -= HandleOnButtonDown;
+       MLInput.OnControllerButtonUp -= HandleOnButtonUp;
+   }
+
+   private void Update() {
+       // Bumper button held down - toggle scanning if timer reaches max
+       if (GetTime() >= TIME_MESH_SCANNING_TOGGLE && BtnState == ButtonStates.Pressed) {
+           _held = true;  
+           _startTime = Time.time;
+           _meshing.ToggleMeshScanning();               
+       }
+       // Bumper was just released - toggle visibility
+       else if (BtnState == ButtonStates.JustReleased) {
+             BtnState = ButtonStates.Normal;
+             _startTime = 0.0f;
+             if (!_held) {
+                _meshing.ToggleMeshVisibility();         
+             } 
+             else {
+                _held = false;
+             }
+       }
+   }
+   #endregion
+
+   #region Private Methods
+   public float GetTime()  {
+       float returnTime = -1.0f;
+       if (_startTime > 0.0f) {
+           returnTime = Time.time - _startTime;
+       }
+       return returnTime;
+   }
+   #endregion
+
+   #region Event Handlers
+   void HandleOnButtonUp(byte controller_id, MLInput.Controller.Button button) {
+       // Callback - Button Up
+       if (button == MLInput.Controller.Button.Bumper) {
+           BtnState = ButtonStates.JustReleased;
+       }
+   }
+
+   void HandleOnButtonDown(byte controller_id, MLInput.Controller.Button button) {
+       // Callback - Button Down
+       if (button == MLInput.Controller.Button.Bumper) {
+           // Start bumper timer
+            _startTime = Time.time;
+           BtnState = ButtonStates.Pressed;
+       }
+   }
+   #endregion
+}
+
---
+++ Unity/Chef's Table/Assets/Scripts/MeshingScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class MeshingScript : MonoBehaviour {
+   #region Public Variables
+   public Material BlackMaterial;
+   public Material GroundMaterial;
+   public Material InactiveMaterial;
+   public MLSpatialMapper Mapper;
+   #endregion
+
+   #region Private Variables
+   private bool _visible = true;
+   #endregion
+
+   #region Unity Methods
+   private void Update() {
+       UpdateMeshMaterial();
+   }
+   #endregion
+
+   #region Public Methods
+   public void ToggleMeshVisibility() {
+       _visible = _visible ? false : true;
+   }
+   public void ToggleMeshScanning() {
+       Mapper.enabled = Mapper.enabled ? false : true;
+   }
+   #endregion
+
+   #region Private Methods
+   /// Switch mesh material based on whether meshing is active and mesh is visible
+   /// visible & active = ground material
+   /// visible & inactive = meshing off material
+   /// invisible = black mesh
+   private void UpdateMeshMaterial() {
+     // Loop over all the child mesh nodes created by MLSpatialMapper script
+     for (int i = 0; i < transform.childCount; i++) {        
+        // Get the child gameObject
+        GameObject gameObject = transform.GetChild(i).gameObject;
+        // Get the meshRenderer component
+        MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        // Get the assigned material
+        Material material = meshRenderer.sharedMaterial;
+        if (_visible) {
+           if (Mapper.enabled) {
+               if (material != GroundMaterial) {
+                   meshRenderer.material = GroundMaterial;
+               }
+           }
+           else if (material != InactiveMaterial) {
+               meshRenderer.material = InactiveMaterial;
+           }
+        }
+        else if (material != BlackMaterial) {
+           meshRenderer.material = BlackMaterial;
+        }
+     }
+   }
+   #endregion
+}
