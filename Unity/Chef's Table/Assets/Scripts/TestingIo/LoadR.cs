6d64fa474adae3b12672c29868a3ddb91b8facb1
Lancial <lx3839663@gmail.com>
2020-05-05 15:03:17 -0700

    testing what's wrong with io

---
+++ Unity/Chef's Table/Assets/Scripts/TestingIo/LoadR.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadR : MonoBehaviour
+{
+    // Start is called before the first frame update
+   // public GameObject resouceLoad;
+    void Start()
+    {
+        GameObject animation = (GameObject)Instantiate(Resources.Load("Animations/tutorial1/step8"), new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
