80af0799e1119c9f30acf77cab431777438eca9d
Lancial <lx3839663@gmail.com>
2020-05-24 21:04:50 -0700

    simulation video play

--- Unity/Chef's Table/Assets/Scripts/meshcutter/changeSimulation.cs
+++ Unity/Chef's Table/Assets/Scripts/meshcutter/changeSimulation.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Video;
 using UnityEngine;
 
 public class changeSimulation : MonoBehaviour
@@ -7,12 +8,25 @@ public class changeSimulation : MonoBehaviour
     // Start is called before the first frame update
     private GameObject slicedObjects;
     public List<GameObject> prefabs;
+    public List<VideoClip> videos; // indice must match with prefabs
+    public GameObject mainCam;
     private int index;
+    private GameObject videoScreen;
+    private VideoPlayer vp;
 
     private void Start()
     {
         slicedObjects = GameObject.Find("CuttingSimulation/SlicedObject");
+        videoScreen = GameObject.Find("SimulationVideoScreen");
+        vp = videoScreen.transform.Find("Screen").GetComponent<VideoPlayer>();
+        
         index = 0;
+    }
+
+    void Update()
+    {
+        videoScreen.transform.LookAt(mainCam.transform.position);
+        
     }
 
     public void nextObject()
@@ -42,6 +56,7 @@ public class changeSimulation : MonoBehaviour
 
     private void updateObject(GameObject prefab)
     {
+        vp.clip = videos[index]; // update video played
         foreach (Transform child in slicedObjects.transform)
         {
             GameObject.Destroy(child.gameObject);
