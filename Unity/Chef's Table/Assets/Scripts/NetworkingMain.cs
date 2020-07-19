a2f528abe9bb9068a1f9ad3e7c132702e6b53271
Lancial <lx3839663@gmail.com>
2020-07-19 16:43:12 -0700

    pipleline code

---
+++ Unity/Chef's Table/Assets/Scripts/NetworkingMain.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Net;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkingMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Texture2D image;
+    private string filename = "\\Users\\wayne\\Desktop\\NetworkingTest\\Assets\\Resources\\kitchen.jpg";
+    private string url = "http://35.225.232.30:5000/predict";
+    private string currResponse = "";
+
+    private int W;
+    private int H;
+    private Raycast rc;
+
+    private void Start()
+    {
+        rc = GameObject.Find("RaycastMain").GetComponent<Raycast>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            byte[] encoded_img = GetFileByteArray(filename);
+            UploadFile(url, encoded_img);
+        }
+        if (!currResponse.Equals(""))
+        {
+            currResponse = "{\"detections\": " + currResponse + " }";
+            DetectionList DL = JsonUtility.FromJson<DetectionList>(currResponse);
+            Dictionary<string, Vector2> rays = new Dictionary<string, Vector2>();
+            foreach(var detection in DL.detections)
+            {
+                int x, y, width, height;
+                x = detection.boxes[0];
+                y = detection.boxes[1];
+                width = detection.boxes[2];
+                height = detection.boxes[3];
+                Vector2 center = getNormalizedCenter(x, y, width, height, W, H);
+                rays[detection.label] = center;
+            }
+            rc.makeRayCast(rays, true);
+        }
+    }
+
+    private void getImageSize(byte[] image)
+    {
+        Texture2D temp = new Texture2D(2, 2);
+        temp.LoadImage(image);
+        W = temp.width;
+        H = temp.height;
+        Debug.Log(W + " " + H);
+    }
+    private byte[] GetFileByteArray(string filename)
+    {
+        FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+        byte[] fileByteArrayData = new byte[fs.Length];
+        fs.Read(fileByteArrayData, 0, System.Convert.ToInt32(fs.Length));
+        fs.Close();
+        getImageSize(fileByteArrayData);
+        return fileByteArrayData;
+    }
+
+    private async void UploadFile(string uri, byte[] rawImage)
+    {
+        WebClient myWebClient = new WebClient();
+        myWebClient.Headers.Add("Content-Type", "binary/octet-stream");
+        myWebClient.Encoding = Encoding.UTF8;
+        byte[] responseArray = await myWebClient.UploadDataTaskAsync(uri, rawImage); ;  // send a byte array to the resource and returns a byte array containing any response
+        currResponse = Encoding.UTF8.GetString(responseArray);
+    }
+    
+    private Vector2 getNormalizedCenter(int x, int y, int width, int height, int W, int H)
+    {
+        return new Vector2((x + width / 2f) / W, (y + height / 2f) / H);
+    }
+
+    [Serializable]
+    public class DetectionList
+    {
+        public List<Detection> detections;
+    }
+
+    [Serializable]
+    public class Detection
+    {
+        public List<int> boxes;
+        public List<int> color;
+        public float confidence;
+        public string label;
+    }
+}
---
+++ Unity/Chef's Table/Assets/Scripts/Raycast.cs
@@ -0,0 +1,76 @@
+﻿using Boo.Lang;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.MagicLeap;
+
+public class Raycast : MonoBehaviour
+{
+
+    public Transform ctransform; // Camera's transform
+    public GameObject prefab;    // Cube prefab
+    public GameObject ImagePlane;
+    //[Range(0, 1)] public float x_ = 0.5f;
+    //[Range(0, 1)] public float y_ = 0.5f;
+
+    private void Update()
+    {
+        
+    }
+
+    public void makeRayCast(Dictionary<string, Vector2> detections, bool debugMode)
+    {
+        if (!MLRaycast.IsStarted)
+        {
+            MLRaycast.Start();
+        }
+        foreach(var detection in detections)
+        {
+            name = detection.Key;
+            float x_ = detection.Value.x;
+            float y_ = detection.Value.y;
+            float x = (x_ - 0.5f) * ImagePlane.transform.localScale.x;
+            float y = (y_ - 0.5f) * ImagePlane.transform.localScale.y;
+            Vector3 local_center = ImagePlane.transform.InverseTransformPoint(0, 0, 0);
+            Vector3 local_point = local_center + new Vector3(x, y, 0);
+            Vector3 world_point = ImagePlane.transform.TransformPoint(local_point);
+            Vector3 direction = world_point - ctransform.position;
+            if (debugMode)  Debug.DrawRay(ctransform.position, direction.normalized, Color.green);
+            MLRaycast.QueryParams _raycastParams = new MLRaycast.QueryParams
+            {
+                Position = ctransform.position,
+                Direction = direction.normalized,
+                UpVector = ctransform.up,
+                Width = 1,
+                Height = 1
+            };
+            MLRaycast.Raycast(_raycastParams, HandleOnReceiveRaycast);
+        }
+    }
+    private void OnDestroy()
+    {
+        MLRaycast.Stop();
+    }
+    // Instantiate the prefab at the given point.
+    // Rotate the prefab to match given normal.
+    // Wait 2 seconds then destroy the prefab.
+    private IEnumerator NormalMarker(Vector3 point, Vector3 normal)
+    {
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, normal);
+        GameObject go = Instantiate(prefab, point, rotation);
+        yield return new WaitForSeconds(0.1f);
+        Destroy(go);
+    }
+
+    // Use a callback to know when to run the NormalMaker() coroutine.
+    void HandleOnReceiveRaycast(MLRaycast.ResultState state, UnityEngine.Vector3 point, Vector3 normal, float confidence)
+    {
+        if (state == MLRaycast.ResultState.HitObserved)
+        {
+            StartCoroutine(NormalMarker(point, normal));
+        }
+    }
+
+    // When the prefab is destroyed, stop MLWorldRays API from running.
+}
