36a11ec8c2022a5e4db86314043a5c483817dbbe
Lancial <lx3839663@gmail.com>
2020-08-07 23:32:22 -0700

    new backend

---
+++ Unity/Chef's Table/Assets/Scripts/chefing2/MainScheduler2.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System;
+using UnityEngine;
+using System.Threading;
+
+public class MainScheduler2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // for bookkeeping and manipulation
+
+    private InstructionList tutorial; // tutorial will be read only
+    private int stepIndex = 0;
+    private List<float> timerRecord = new List<float>();
+    private bool timerPause = true;
+
+    // for statistic
+    private string chosenRecipe = "";
+    private float totalTime = 0;
+
+
+    // global states
+    private bool tutorialStarts = false; // indicate if a user has choosen a tutorial
+    private bool tutorialFinish = false;
+
+
+
+    // simply reset everything.
+    public void reset()
+    {
+        tutorial = null;
+        stepIndex = 0;
+        timerRecord.Clear();
+        chosenRecipe = "";
+        totalTime = 0;
+        tutorialStarts = false;
+        tutorialFinish = false;
+    }
+
+    public void addToTimer(float delta)
+    {
+        if (tutorialStarts)
+        {
+            timerRecord[stepIndex] += delta;
+        }
+    }
+
+    public void subtractFromTimer(float delta)
+    {
+        if (tutorialStarts)
+        {
+            timerRecord[stepIndex] -= delta;
+        }
+    }
+
+    // change timer status at the current step index for all substeps
+    // 0 for pause, 1 for start, 2 reset timer and pause
+    public void changeTimerStatus(bool pause)
+    {
+        timerPause = pause;
+    }
+
+    public bool isTutorialDone()
+    {
+        return tutorialFinish;
+    }
+
+    public List<string> getSummary()
+    {
+        List<String> res = new List<string>();
+        res.Add(chosenRecipe);
+        res.Add(GetTimeSpanWithSec(totalTime));
+        return res;
+    }
+
+    // return a map of all info of the current step, null if no tutorial is selected
+    public Dictionary<string, List<string>> getCurrentStepInfo()
+    {
+        if (!tutorialStarts) return null;
+        Dictionary<string, List<string>> dic = new Dictionary<string, List<string>>();
+        // TODO: add utensils and ingredients
+        dic.Add("description", new List<string>() { tutorial.steps[stepIndex].step});
+        dic.Add("timer", new List<string>() { GetTimeSpanWithSec(tutorial.steps[stepIndex].timer) });
+        dic.Add("recipe", new List<string>() { chosenRecipe });
+        dic.Add("StepNum", new List<string>() { (stepIndex + 1) + "" });
+        return dic;
+    }
+
+    // proceed to the next task in the list
+    public void toNextStep()
+    {
+        stepIndex++;
+        if (stepIndex >= timerRecord.Count)
+        {
+            tutorialFinish = true;
+        }
+    }
+
+    public void toPreviousStep()
+    {
+        resetTimerRecord();
+        if (stepIndex == 0) return;
+        stepIndex--;
+        tutorialFinish = false;
+    }
+
+    private void resetTimerRecord()
+    {
+        if (tutorial != null)
+        {
+            timerRecord.Clear();
+            foreach(Instruction instruction in tutorial.steps)
+            {
+                timerRecord.Add(instruction.timer);
+            }
+            timerPause = true;
+        }
+    }
+
+    // for user interface to call when a user select a recipe
+    // name: name of the recipe
+    public void startTutorial(StepsList wrapper)
+    {
+        try
+        {
+            tutorial = wrapper.result[0];
+            tutorialStarts = true;
+            resetTimerRecord();
+        }
+        catch (NullReferenceException e)
+        {
+            Debug.LogWarning(e);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tutorialStarts)
+        {
+            if (!timerPause)
+            {
+                timerRecord[stepIndex] = Math.Max(timerRecord[stepIndex] - Time.deltaTime, 0);
+                
+            }
+        }
+
+    }
+
+
+    // for visualizing timers
+    private string GetTimeSpanWithSec(float seconds)
+    {
+        TimeSpan interval = TimeSpan.FromSeconds(Math.Floor(seconds));
+        return interval.ToString();
+    }
+
+}
---
+++ Unity/Chef's Table/Assets/Scripts/chefing2/SaveToDevice.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class SaveToDevice : MonoBehaviour
+{
+    private string record = "/documents/C2/record.txt";
+    private string c2_path = "/documents/C2/";
+    private string towrite = "hello";
+    private float timer = 5;
+
+    private void Start()
+    {
+        if (!File.Exists(record))
+        {
+            string init = "";
+            File.WriteAllText(record, init);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            timer = 5;
+            Debug.Log("2" + Directory.Exists("/documents/C2"));
+            File.WriteAllText(c2_path + "hello.json", towrite);
+            Debug.Log(File.Exists(c2_path + "hello.json"));
+            string toprint = File.ReadAllText(c2_path + "hello.json");
+            Debug.Log(toprint);
+            saveRecipe("food", "requirements", "instructions", new byte[0], new List<byte[]>()); ;
+        }
+
+    }
+
+    // name : name of the recipe
+    // json : raw json string we get from the web, as long as we can deserialize
+    // images: a image for each steps
+    public void saveRecipe(string name, string requirments, string instructions, byte[] cover_image, List<byte[]> step_images)
+    {
+        
+        if (Directory.Exists(c2_path + name)) return;
+        string folder_path = c2_path + name + "/";
+        File.WriteAllText(folder_path + "requirements.txt", requirments);
+        File.WriteAllText(folder_path + "instructions.txt", instructions);
+        File.WriteAllBytes(folder_path + "cover.jpg", cover_image);
+        for (int i = 0; i < step_images.Count; i++)
+        {
+            File.WriteAllBytes(folder_path + "step" + i + ".jpg", step_images[i]);
+        }
+        string record_string = File.ReadAllText(record);
+        record_string += name + ",";
+        File.WriteAllText(record, record_string);
+    }
+
+    public List<previewRecipe> getRecipeList()
+    {
+        string record_string = File.ReadAllText(record);
+        List<string> folders = new List<string>();
+        string[] words = record_string.Split(' ');
+        for (int i = 0; i < words.Length; i++)
+        {   
+            if (words[i] != "")
+            {
+                folders.Add(words[i]);
+            }
+            
+        }
+        return null;
+    }
+
+    public StoredRecipe getSingleRecipe()
+    {
+        return null;
+    }
+
+    public class previewRecipe
+    {
+        private string name;
+        private string requirements;
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getRequirements()
+        {
+            return requirements;
+        }
+    }
+    public class StoredRecipe
+    {
+        private string name;
+        private string requirements;
+        private string instuctions;
+        private List<byte[]> images;
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getRequirements()
+        {
+            return requirements;
+        }
+
+        public string getInstructions()
+        {
+            return instuctions;
+        }
+
+        private List<byte[]> getImageOfStep()
+        {
+            return images;
+        }
+    }
+}
